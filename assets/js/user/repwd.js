17b617b8d90a70cd9e11698e46a8a26504094c87
feimao <aaaa@qq.com>
2020-06-10 12:46:58 +0800

    完成表单密码修改重置功能

--- assets/js/user/repwd.js
+++ assets/js/user/repwd.js
@@ -1,6 +1,6 @@
 //入口函数
 $(function () {
-    //---------------------------表单验证-------------------------
+    //---------------------------表单验证功能-------------------------
     /* 思路：
         1.新密码与旧密码不能相同
         2.密码长度不能小于6位数，且不能大于12位数
@@ -25,13 +25,42 @@ $(function () {
             //获取旧密码
             var newPwd = $('.newPwd').val(); //不能使用this，this指向的是form表单
             //判断
-            if (newPwd != val) {
+            if (val !== newPwd) {
                 return '新密码与确认密码要保持一致哦~';
             }
         },
     });
 
-    //-------------------------完成密码重置----------------------------
-    
-
+    //-------------------------完成密码重置功能----------------------------
+    $('form').on('submit', function (e) { 
+        e.preventDefault();
+        // ajax提交原密码和新密码
+        $.ajax({
+            type: 'POST',
+            url: 'http://www.liulongbin.top:3007/my/updatepwd',
+            data: $(this).serialize(), // 使用serialize的时候，一定要检查表单各项的name
+            success: function (res) {
+                // 给一个提示
+                layer.msg(res.message);
+                if (res.status === 0) {
+                    // 更新成功，重置表单
+                    // reset方法，可以重置表单，但是他是一个DOM方法，所以需要把jQuery对象转成DOM对象
+                    $('form')[0].reset();
+                }
+            },
+            headers: {
+                'Authorization': localStorage.getItem('token')
+            },
+            complete: function (xhr) {
+                if (xhr.responseJSON.status === 1 && xhr.responseJSON.message === '身份认证失败！') {
+                    // 清楚过期的token或者无效的token
+                    localStorage.removeItem('token');
+                    // 跳转到登录页
+                    // window 表示当前的窗口，即repwd.html
+                    // window.parent 表示当前窗口的父窗口，即index.html
+                    window.parent.location.href = '/login.html';
+                }
+            }
+        });
+    });
 })
